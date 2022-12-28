24dbe49b2068ee017cafb21367e2dfe906949492
tamutamu0 <tamutamu0@users.noreply.github.com>
2022-12-29 00:03:46 +0900

    add Header

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -1,14 +1,103 @@
 import React, { memo } from "react";
+import { styled, alpha } from '@mui/material/styles';
+import {
+    AppBar,
+    Box,
+    Toolbar,
+    IconButton,
+    InputBase,
+    Typography,
+    Menu,
+    Container,
+    Avatar,
+    Button,
+    Tooltip,
+    MenuItem,
+    Stack,
+} from "@mui/material"
+import PetsRoundedIcon from '@mui/icons-material/PetsRounded';
+import SearchIcon from '@mui/icons-material/Search';
 
-type Props = {};
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+    },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
+    },
+}));
 
 // eslint-disable-next-line react/display-name
 export const Header = memo(() => {
     return (
-        <>
-            <div>
-                aa
-            </div>
-        </>
+        <AppBar position="static">
+            <Container maxWidth="lg">
+                <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Stack
+                        direction="row"
+                        alignItems="center"
+                    >
+                        <PetsRoundedIcon sx={{ mr: 1 }} />
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="a"
+                            href="/"
+                            sx={{
+                                fontFamily: 'monospace',
+                                fontWight: 700,
+                                letterSpacing: '.3rem',
+                                color: 'inherit',
+                                textDecoration: 'none',
+                            }}
+                        >
+                            タムボーblog
+                        </Typography>
+                    </Stack>
+                    <Search>
+                        <SearchIconWrapper>
+                            <SearchIcon />
+                        </SearchIconWrapper>
+                        <StyledInputBase
+                            placeholder="Search"
+                            inputProps={{ 'aria-label': 'serach' }}
+                        />
+                    </Search>
+                </Toolbar>
+            </Container>
+        </AppBar>
     )
 });
