845ddf5cfb59363a61d866e80c5c3eed4188f7f2
tamutamu0 <tamutamu0@users.noreply.github.com>
2023-03-07 01:04:35 +0900

    fix tsエラー　とりあえずany付けた

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -38,12 +38,12 @@ export default function BlogId({ blog }: props) {
 export const getStaticPaths = async () => {
     const data = await client.get({ endpoint: "blog" });
 
-    const paths = data.contents.map((content) => `/blog/${content.id}`);
+    const paths = data.contents.map((content: any) => `/blog/${content.id}`);
     return { paths, fallback: false };
 };
 
 // データをテンプレートに受け渡す部分の処理を記述します
-export const getStaticProps = async (context) => {
+export const getStaticProps = async (context: any) => {
     const id = context.params.id;
     const data = await client.get({ endpoint: "blog", contentId: id });
 
