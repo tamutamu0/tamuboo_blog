cdced3a6dec628e2331dc67ab5b689a98779e0f6
tamutamu0 <tamutamu0@users.noreply.github.com>
2023-03-12 04:54:52 +0900

    add カスタムheadコンポーネントの作成

---
+++ components/elements/MyHead.tsx
@@ -0,0 +1,27 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+interface MyHeadProps {
+    pageTitle?: string
+    pageThumbnailUrl?: string
+    pageDescription?: string
+}
+
+const MyHead: NextPage<MyHeadProps> = ({ pageTitle, pageThumbnailUrl, pageDescription }) => {
+    const defaultTitle = "タムボー"
+
+    const title = pageTitle ? `${pageTitle} | ${defaultTitle} : ${defaultTitle}` : defaultTitle
+    const thumbnailUrl = pageThumbnailUrl
+    const description = pageDescription
+
+    return (
+        <Head>
+            <title>{title}</title>
+            <meta property="og:title" content={title} />
+            <meta property="og:image" content={thumbnailUrl} />
+            <meta property="og:description" content={description} />
+        </Head>
+    )
+}
+
+export default MyHead
