3eab8f96f6cf6ee720958ead4d49c5d6867071b3
tamutamu0 <tamutamu0@users.noreply.github.com>
2023-03-12 04:55:56 +0900

    add 記事タグ表示の追加

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,18 +1,61 @@
 import Link from "next/link";
 import Head from "next/head";
+import Image from "next/image";
 import { Header } from "../../components/layouts/Header";
 // libs
 import { client } from "../../libs/microcmsClient";
 // types
 import type { Blog, Tag } from "../../types/blog";
 // mui
-import { Box, Container, Typography } from "@mui/material";
+import { Box, BoxProps, Container, Grid, Typography } from "@mui/material";
+import { Stack } from "@mui/system";
+// 
+import { getDateStr } from "../../utils/getDateStr";
 
-type props = {
+type Props = {
     blog: Blog
 }
 
-export default function BlogId({ blog }: props) {
+interface TagListProps extends BoxProps {
+    tags: Tag[];
+}
+
+const TagList = (props: TagListProps) => {
+    const { tags, ...rest } = props;
+    return (
+        <Stack
+            direction="row"
+            spacing={1}
+        >
+            {
+                tags.map(tag => (
+                    <Link key={tag.id} href={`/topics/${tag.name}`} passHref >
+                        <Stack
+                            direction="row"
+                            spacing={1}
+                            sx={{
+                                p: 0.8,
+                                border: '1px solid',
+                                borderColor: 'lightgray',
+                                borderRadius: '20px',
+                                maxWidth: "fit-content"
+                            }}
+                        >
+                            <Image
+                                src={tag.icon}
+                                alt=""
+                                width={20}
+                                height={25} />
+                            <Typography variant="body2">{tag.name}</Typography>
+                        </Stack>
+                    </Link>
+                ))
+            }
+        </Stack>
+    )
+}
+
+export default function BlogId({ blog }: Props) {
     return (
         <>
             <Container>
@@ -20,15 +63,37 @@ export default function BlogId({ blog }: props) {
                     <Typography variant="h4" component="h1" gutterBottom>
                         {blog.title}
                     </Typography>
-                    <p>{blog.publishedAt}</p>
-                    <div
-                        dangerouslySetInnerHTML={{
-                            __html: `${blog.content}`,
+                    <p>{getDateStr(blog.publishedAt)}</p>
+                    <Box
+                        sx={{
+                            bgcolor: "background.paper",
+                            borderRadius: "12px",
+                            p: {
+                                xs: 1,
+                                sm: 2,
+                                md: 4,
+                                lg: 6,
+                                xl: 8,
+                            }
                         }}
-                    />
+                    >
+                        <TagList
+                            tags={blog.tags}
+                        />
+                        <Box
+                            sx={{
+                                pt: {
+                                    xs: 1,
+                                    sm: 2,
+                                    md: 4,
+                                    lg: 6,
+                                    xl: 8,
+                                }
+                            }}
+                            dangerouslySetInnerHTML={{ __html: `${blog.content}` }} />
+                    </Box>
                 </main>
-            </Container>
-
+            </Container >
         </>
     );
 
