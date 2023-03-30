dfd01337c7eb5998efb87b193f408487228119e9
tamutamu0 <tamutamu0@users.noreply.github.com>
2023-03-30 22:06:28 +0900

    fix デバイスごとの余白

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,18 @@
-import 'tailwindcss/tailwind.css';
-import type { AppProps } from 'next/app'
+import "tailwindcss/tailwind.css";
+import type { AppProps } from "next/app";
 import "types/globals.css";
-import Head from 'next/head';
-import { Header } from '../components/layouts/Header';
-import { useState } from 'react';
-import { Container } from '@mui/material';
+import Head from "next/head";
+import { Header } from "../components/layouts/Header";
+import { useState } from "react";
+import { Container } from "@mui/material";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
-      <Container sx={{ p: 4 }}>
+      <Container sx={{ p: { xs: 2, sm: 4 } }}>
         <Component {...pageProps} />
       </Container>
     </>
-  )
+  );
 }
--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -4,79 +4,114 @@ import { client } from "../../libs/microcmsClient";
 import type { Blog } from "../../types/blog";
 // mui
 import { Box, Container, Typography } from "@mui/material";
-import {load as cheerioLoad} from "cheerio";
-// 
+import { load as cheerioLoad } from "cheerio";
+//
 import { getDateStr } from "../../utils/getDateStr";
 import MyHead from "../../components/elements/MyHead";
 import TagList from "../../components/layouts/TagList";
 
 type Props = {
-    blog: Blog
-}
+  blog: Blog;
+};
 
 export default function BlogId({ blog }: Props) {
-    return (
-        <>
-            <MyHead
-                pageTitle={blog.title}
-                pageThumbnailUrl={blog.image.url}
-            />
-            <Container>
-                <main>
-                    <Typography variant="h4" component="h1" gutterBottom>
-                        {blog.title}
-                    </Typography>
-                    <p>{getDateStr(blog.publishedAt)}</p>
-                    <Box
-                        sx={{
-                            bgcolor: "background.paper",
-                            borderRadius: "12px",
-                            p: 4
-                        }}
-                    >
-                        <TagList
-                            tags={blog.tags}
-                        />
-                        <Box
-                            sx={{
-                                pt: 3
-                            }}
-                            dangerouslySetInnerHTML={{ __html: `${blog.content}` }} />
-                    </Box>
-                </main>
-            </Container >
-        </>
-    );
-
+  return (
+    <Box>
+      <MyHead pageTitle={blog.title} pageThumbnailUrl={blog.image.url} />
+      <Container
+        sx={{
+          display: "flex",
+          px: {
+            xs: 0,
+            sm: 2,
+            md: 4,
+            lg: 6,
+            xl: 8,
+          },
+        }}
+      >
+        <Box sx={{ flex: 1, p: 0 }}>
+          <main>
+            <Typography variant="h5" component="h1" gutterBottom>
+              {blog.title}
+            </Typography>
+            <p>{getDateStr(blog.publishedAt)}</p>
+            <Box
+              sx={{
+                bgcolor: "background.paper",
+                borderRadius: "12px",
+                boxShadow: 1,
+                p: 1,
+              }}
+            >
+              <TagList tags={blog.tags} />
+              <Box
+                sx={{
+                  pt: 1,
+                  px: {
+                    xs: 0,
+                    sm: 2,
+                    md: 4,
+                    lg: 6,
+                    xl: 8,
+                  },
+                }}
+                dangerouslySetInnerHTML={{ __html: `${blog.content}` }}
+              />
+            </Box>
+          </main>
+        </Box>
+        {/* サイドバー */}
+        {/* <Box
+          sx={{
+            display: { xs: "none", md: "block" }, // スマートフォンでは非表示、md以上で表示
+            width: "250px",
+            ml: 3,
+            bgcolor: "white",
+            borderRadius: "12px",
+            boxShadow: 1,
+            p: 2,
+          }}
+        >
+          <Typography variant="h5" component="h2">
+            Sidebar
+          </Typography>
+          <Box>サイドバーコンテンツ</Box>
+        </Box> */}
+      </Container>
+    </Box>
+  );
 }
 
 // 静的生成のためのパスを指定します
 export const getStaticPaths = async () => {
-    const data = await client.get({ endpoint: "blog" });
+  const data = await client.get({ endpoint: "blog" });
 
-    const paths = data.contents.map((content: any) => `/blog/${content.id}`);
-    return { paths, fallback: false };
+  const paths = data.contents.map((content: any) => `/blog/${content.id}`);
+  return { paths, fallback: false };
 };
 
 // データをテンプレートに受け渡す部分の処理を記述します
 export const getStaticProps = async (context: any) => {
-    const id = context.params.id;
-    const data = await client.get({ endpoint: "blog", contentId: id });
+  const id = context.params.id;
+  const data = await client.get({ endpoint: "blog", contentId: id });
 
-    //htmlパース処理
-    const $ = cheerioLoad(data.content);
-    $("h1,h2").each((_,element) => {
-        $(element).addClass("text-2xl mt-5 mb-3 font-bold")
-    });
-    $("a").each((_,element) => {
-        $(element).addClass("text-blue-600 hover:text-blue-800 visited:text-purple-600")
-    });
-    data.content = $.html();
-    console.log($.html());
+  //htmlパース処理
+  const $ = cheerioLoad(data.content);
+  $("h1,h2").each((_, element) => {
+    $(element).addClass("text-2xl mt-5 mb-3 font-bold");
+  });
+  $("a").each((_, element) => {
+    $(element).addClass(
+      "text-blue-600 hover:text-blue-800 visited:text-purple-600"
+    );
+  });
+  data.content = $.html();
+  console.log($.html());
 
-    return {
-        props: {
-            blog: data,
-        },
-    };
+  return {
+    props: {
+      blog: data,
+    },
+  };
 };
