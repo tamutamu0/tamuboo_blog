cac4ba5da6685aeaf7f4485eddfb613d17cf81f5
tamutamu0 <tamutamu0@users.noreply.github.com>
2024-04-07 00:00:13 +0900

    format

--- components/elements/MyHead.tsx
+++ components/elements/MyHead.tsx
@@ -1,27 +1,31 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from "next";
+import Head from "next/head";
 
 interface MyHeadProps {
-    pageTitle?: string
-    pageThumbnailUrl?: string
-    pageDescription?: string
+  pageTitle?: string;
+  pageThumbnailUrl?: string;
+  pageDescription?: string;
 }
 
-const MyHead: NextPage<MyHeadProps> = ({ pageTitle, pageThumbnailUrl, pageDescription }) => {
-    const defaultTitle = "タムボー"
+const MyHead: NextPage<MyHeadProps> = ({
+  pageTitle,
+  pageThumbnailUrl,
+  pageDescription,
+}) => {
+  const defaultTitle = "たむぼー";
 
-    const title = pageTitle ? `${pageTitle} | ${defaultTitle} ` : defaultTitle
-    const thumbnailUrl = pageThumbnailUrl
-    const description = pageDescription
+  const title = pageTitle ? `${pageTitle} | ${defaultTitle} ` : defaultTitle;
+  const thumbnailUrl = pageThumbnailUrl;
+  const description = pageDescription;
 
-    return (
-        <Head>
-            <title>{title}</title>
-            <meta property="og:title" content={title} />
-            <meta property="og:image" content={thumbnailUrl} />
-            <meta property="og:description" content={description} />
-        </Head>
-    )
-}
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta property="og:title" content={title} />
+      <meta property="og:image" content={thumbnailUrl} />
+      <meta property="og:description" content={description} />
+    </Head>
+  );
+};
 
-export default MyHead
+export default MyHead;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,29 +4,35 @@ import { client } from "../libs/microcmsClient";
 // types
 import type { Blog } from "../types/blog";
 // mui
-import { Box, CardMedia, Container, Grid, styled, Typography } from "@mui/material";
+import {
+  Box,
+  CardMedia,
+  Container,
+  Grid,
+  styled,
+  Typography,
+} from "@mui/material";
 // components
 import MyHead from "../components/elements/MyHead";
-import Pagination from "../components/elements/Pagination"
+import Pagination from "../components/elements/Pagination";
 import { PER_PAGE } from "../setting";
 
 type Props = {
-  blogs: Blog[],
-  totalCount: number,
-}
+  blogs: Blog[];
+  totalCount: number;
+};
 
-const BlogPaper = styled('div')({
-  padding: '1px',
-  backgroundColor: 'white',
-  borderRadius: '15px',
-  transition: 'all 0.2s',
-  boxShadow: '3px 3px 20px -10px #777777',
-  '&:hover': {
-    boxShadow:
-      '3px 3px 20px -5px #777777',
-    transform: 'translateY(-1px)',
+const BlogPaper = styled("div")({
+  padding: "1px",
+  backgroundColor: "white",
+  borderRadius: "15px",
+  transition: "all 0.2s",
+  boxShadow: "3px 3px 20px -10px #777777",
+  "&:hover": {
+    boxShadow: "3px 3px 20px -5px #777777",
+    transform: "translateY(-1px)",
   },
-  height: '100%'
+  height: "100%",
 });
 
 export default function Home({ blogs, totalCount }: Props) {
@@ -36,31 +42,28 @@ export default function Home({ blogs, totalCount }: Props) {
       <Grid container spacing={4}>
         {blogs.map((blog) => (
           <Grid item xs={12} sm={6} md={4} lg={4} key={blog.id}>
-            <Link href={`/blog/${blog.id}`} passHref >
+            <Link href={`/blog/${blog.id}`}>
               <BlogPaper>
-                <Box sx={{ borderRadius: '15px 15px 0px 0px', overflow: 'hidden' }}>
+                <Box
+                  sx={{ borderRadius: "15px 15px 0px 0px", overflow: "hidden" }}
+                >
                   <CardMedia
-                    component='img'
-                    width='100%'
-                    height='auto'
+                    component="img"
+                    width="100%"
+                    height="auto"
                     image={blog.image.url}
                     alt={blog.image.url}
                   />
                 </Box>
-                <Container sx={{ paddingTop: '10px' }}>
-                  <Typography variant="subtitle1">
-                    {blog.title}
-                  </Typography>
+                <Container sx={{ paddingTop: "10px" }}>
+                  <Typography variant="subtitle1">{blog.title}</Typography>
                 </Container>
               </BlogPaper>
             </Link>
           </Grid>
         ))}
       </Grid>
-      <Pagination
-        currentPage={1}
-        totalCount={totalCount}
-      />
+      <Pagination currentPage={1} totalCount={totalCount} />
     </>
   );
 }
@@ -70,13 +73,13 @@ export const getStaticProps = async () => {
     endpoint: "blog",
     queries: {
       offset: 0,
-      limit: PER_PAGE
-    }
+      limit: PER_PAGE,
+    },
   });
   return {
     props: {
       blogs: blog.contents,
-      totalCount: blog.totalCount
-    }
+      totalCount: blog.totalCount,
+    },
   };
-}
+};
